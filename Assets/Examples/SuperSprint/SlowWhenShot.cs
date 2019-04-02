52e5e47175c9d52000ea02528f660ae4328426e9
David Sharrock <42804075+dsharrock-acm@users.noreply.github.com>
2019-04-02 17:30:25 +0100

    Updated Super Sprint Example Scene with Lasers

---
+++ Assets/Examples/SuperSprint/SlowWhenShot.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class SlowWhenShot : MonoBehaviour
+{
+    Rigidbody2D rb2d;
+    [SerializeField] float slowFactor = 1.0f;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+    }
+
+    public void SlowByPercent()
+    {
+        Debug.Log("I'm HIT!");
+        rb2d.velocity = rb2d.velocity * (slowFactor / 100);
+    }
+
+    public void SlowByImpulse()
+    {
+        Debug.Log("I'm HIT!");
+        rb2d.AddForce(-transform.up * slowFactor, ForceMode2D.Force);
+    }
+
+}
