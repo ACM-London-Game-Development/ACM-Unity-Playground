01f2cc320b0e7e4197adbd4940999c283e2c3bb4
David Sharrock <42804075+dsharrock-acm@users.noreply.github.com>
2019-04-02 17:27:37 +0100

    Added Message Action
    Added Message Action to LoadLevelAction so that I can call it with SendMessage

--- Assets/Scripts/Conditions/Actions/LoadLevelAction.cs
+++ Assets/Scripts/Conditions/Actions/LoadLevelAction.cs
@@ -9,6 +9,19 @@ public class LoadLevelAction : Action
 
 	public const string SAME_SCENE = "0";
 	
+    public override void MessageAction()
+    {
+        if (levelName == SAME_SCENE)
+        {
+            //just restart the level
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name, LoadSceneMode.Single);
+        }
+        else
+        {
+            //load another scene
+            SceneManager.LoadScene(levelName, LoadSceneMode.Single);
+        }
+    }
 
 	//Loads a new Unity scene, or reload the current one (it means all objects are reset)
 	public override bool ExecuteAction(GameObject dataObject)
--- Assets/_INTERNAL_/Scripts/BaseClasses/Action.cs
+++ Assets/_INTERNAL_/Scripts/BaseClasses/Action.cs
@@ -4,7 +4,12 @@ using System;
 
 public abstract class Action : MonoBehaviour
 {
-	public virtual bool ExecuteAction(GameObject other)
+    public virtual void MessageAction()
+    {
+
+    }
+
+    public virtual bool ExecuteAction(GameObject other)
 	{
 		//the return value indicates if the action has been successful
 		//some actions always return true
