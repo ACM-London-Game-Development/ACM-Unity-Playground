65ff450fbf11cb54cd1d5c28c58c44c8a2e5b1e1
David Sharrock <42804075+dsharrock-acm@users.noreply.github.com>
2019-04-02 17:28:42 +0100

    Added Audio Panning Script

---
+++ Assets/Scripts/Gameplay/PanObjectAudio.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+public class PanObjectAudio : MonoBehaviour
+{
+    AudioSource audioSource;
+    Camera cam;
+    Vector3 screenPos;
+    float halfScreenWidth;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        audioSource = GetComponent<AudioSource>();
+        cam = Camera.main;
+        halfScreenWidth = cam.scaledPixelWidth / 2;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        screenPos = cam.WorldToScreenPoint(transform.position);
+        audioSource.panStereo = -(halfScreenWidth - screenPos.x) / halfScreenWidth;
+    }
+}
